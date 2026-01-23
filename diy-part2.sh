a980372efc700d6a4711c7cc4e45e69ddb761ade
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-01-23 16:45:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,15 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#!/bin/bash
+
+# 1. 修改默认 IP 为 192.168.10.1 (避免与光猫 192.168.1.1 冲突)
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 2. 修改默认主机名为 Home-Router
+sed -i 's/OpenWrt/Home-Router/g' package/base-files/files/bin/config_generate
+
+# 3. 针对美的、盼盼等智能家居优化（允许本地多播发现）
+# 开启 mDNS 基础支持
+echo "CONFIG_PACKAGE_avahi-dbus-daemon=y" >> .config
+echo "CONFIG_PACKAGE_libavahi-dbus-support=y" >> .config
