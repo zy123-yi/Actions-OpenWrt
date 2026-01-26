366535457f418183f7afcb5b5b8143ee76abdc86
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-01-26 15:28:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,54 +1,42 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-#!/bin/bash
-
-rm -rf ./tmp
-
-# 1. 修改默认 IP 为 192.168.10.1 (避免与光猫 192.168.1.1 冲突)
+# 1. 基础信息修改
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# 2. 修改默认主机名为 Home-Router
 sed -i 's/OpenWrt/Home-Router/g' package/base-files/files/bin/config_generate
 
-# 3. 针对美的、盼盼等智能家居优化（允许本地多播发现）
-# 开启 mDNS 基础支持
-echo "CONFIG_PACKAGE_avahi-dbus-daemon=y" >> .config
-echo "CONFIG_PACKAGE_libavahi-dbus-support=y" >> .config
-#!/bin/bash
+# 2. 物理移除会导致编译失败的“钉子户”插件
+# 彻底解决 v2ray-plugin, geoview, shadowsocks-rust, hysteria 等冲突
+declare -a bad_pkgs=(
+    "feeds/helloworld/v2ray-plugin"
+    "feeds/helloworld/shadowsocks-rust"
+    "feeds/helloworld/hysteria"
+    "feeds/helloworld/luci-app-hysteria"
+    "feeds/passwall_packages/geoview"
+    "feeds/passwall_packages/hysteria"
+    "feeds/passwall_packages/hysteria2"
+    "feeds/passwall_packages/shadowsocks-rust"
+    "feeds/passwall_packages/tuic-client"
+    "feeds/passwall_packages/naiveproxy"
+)
 
-# 1. 屏蔽 Hysteria (导致你目前报错的主要原因)
-rm -rf feeds/helloworld/hysteria
-rm -rf feeds/helloworld/luci-app-hysteria
-rm -rf feeds/passwall_packages/hysteria
-rm -rf feeds/passwall_packages/hysteria2
+for pkg in "${bad_pkgs[@]}"; do
+    rm -rf "$pkg"
+    # 同时删除已安装的软链接，防止 No rule 报错
+    find package/feeds/ -name "$(basename $pkg)" -type l -delete
+done
 
-# 2. 屏蔽其他潜在的 Go 冲突包
-rm -rf feeds/helloworld/tuic-client
-rm -rf feeds/helloworld/naiveproxy
-rm -rf feeds/passwall_packages/tuic-client
-rm -rf feeds/passwall_packages/naiveproxy
+# 3. 智能家居优化：增强 mDNS/Avahi 发现能力
+# 这能显著提升手机 App 发现美的空调和追觅扫地机的成功率
+{
+    echo "CONFIG_PACKAGE_avahi-dbus-daemon=y"
+    echo "CONFIG_PACKAGE_libavahi-dbus-support=y"
+    echo "CONFIG_PACKAGE_kmod-nft-tproxy=y"
+    echo "CONFIG_PACKAGE_kmod-nft-socket=y"
+} >> .config
 
-# 3. 屏蔽 Rust 报错包 (如果 Rust 也报错，建议关掉这个不常用的)
-rm -rf feeds/packages/lang/rust
-rm -rf feeds/packages/utils/onionshare-cli
-
-# 4. 强制清理残留
+# 4. 强制修复包索引（解决 No rule to make target）
+./scripts/feeds update -i
 ./scripts/feeds install -a
+
+# 5. 最后的强力清洗：清除可能存在的残留 .config 干扰
+rm -rf tmp
