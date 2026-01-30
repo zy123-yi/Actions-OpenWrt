f12afd76a673ff348a08db08070da6762b96d406
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-01-30 09:32:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,13 +2,15 @@
 
 # 1. 强制重写 feeds.conf.default 和 feeds.conf，改用 GitHub 镜像源并彻底移除 helloworld
 cat > feeds.conf.default <<EOF
-src-git packages https://github.com/openwrt/packages.git;openwrt-24.10
-src-git luci https://github.com/openwrt/luci.git;openwrt-24.10
-src-git routing https://github.com/openwrt/routing.git;openwrt-24.10
-src-git telephony https://github.com/openwrt/telephony.git;openwrt-24.10
+src-git packages https://git.openwrt.org/feed/packages.git^f1470815f41fe199bfdafe9a91ad115bc2e91203
+src-git luci https://git.openwrt.org/project/luci.git^75e41cba5160281b2d1dca922719efef29f7ffd8
+src-git routing https://git.openwrt.org/feed/routing.git^3eb59e9471858c83891979793f1dd29cca156919
+src-git telephony https://git.openwrt.org/feed/telephony.git^2a4541d46199ac96fac214d02c908402831c4dc6
 src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
 src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
-src-git ower https://github.com/sbwml/luci-app-mosdns
+# src-git helloworld https://github.com/fw876/helloworld.git
+src-git-full kenzok8 https://github.com/kenzok8/openwrt-packages
+src-git-full small https://github.com/kenzok8/small
 EOF
 
 # 同步覆盖 feeds.conf（防止某些 Actions 模板优先读取此文件）
