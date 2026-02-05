46e0bee2152a96b0e180127fc9eb2d94aeec12de
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-02-06 00:05:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # 彻底清空所有第三方源，防止 feeds 脚本去尝试克隆它们
 
+#!/bin/bash
+# 1. 添加 helloworld (SSR Plus+) 源
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
+# 2. 如果你还想要更多插件（可选），可以添加这个常用库
+# echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
