7525a820754729ab0576e7fd488cb65273b522f7
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-22 12:46:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# 移除自带的旧包，防止名称冲突导致的不更新
-rm -rf package/lean/luci-app-passwall
+# 1. 彻底删除 feeds.conf.default 中已有的相关条目（防止重复定义导致 merging error）
+sed -i '/passwall/d' feeds.conf.default
+sed -i '/daed/d' feeds.conf.default
+
+# 2. 重新添加官方最前线源码仓库
+# 使用不同的名称（如 passwall_official）可以降低冲突风险
+echo "src-git passwall_pkgs https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+echo "src-git daed_repo https://github.com/QiuSimons/luci-app-daed.git" >> feeds.conf.default
+
+# 3. 额外保险：如果 LEDE 源码 package 目录下有残留的同名文件夹，直接删除
+rm -rf package/feeds/passwall*
 rm -rf package/feeds/luci/luci-app-passwall
-
-# 添加 PassWall 官方源 (luci + packages)
-# 这样做能确保你在“服务”菜单里看到的永远是 xiaorouji 的最新版本
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '$a src-git passwall_pkgs https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-
-# 提示：daed 的源也可以在这里一并处理
-rm -rf package/feeds/daed/luci-app-daed
+rm -rf package/feeds/daed
