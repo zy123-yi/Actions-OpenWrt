4b77a1c7870b6ff25e33c2f73b8d1857b66e6d4c
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 13:08:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,3 @@ echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' >> fe
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
