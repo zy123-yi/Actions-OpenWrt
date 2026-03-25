aa3b899cac2b127e833ea133095deddfaa70b169
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 22:25:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-# 强制将固件身份修改为最新的稳定版号
-# 这样 apk 就会自动去 /releases/25.12.3/ 路径下载插件
+# 假设在 openwrt 目录执行
+VERSION_VAL="25.12.1"
 
-if [ -n "$VERSION_NUMBER" ]; then
-    echo "Updating version to $VERSION_NUMBER"
-    # 修改 version.mk 里的版本定义
-    sed -i "s/VERSION_NUMBER:=.*/VERSION_NUMBER:=${VERSION_NUMBER}/g" include/version.mk
-    # 修改发布描述
-    sed -i "s/DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='OpenWrt ${VERSION_NUMBER} Stable'/g" package/base-files/files/etc/openwrt_release
-fi
-# 1. 从 feeds 源码中彻底删除会导致报错的 trojan 相关包
-rm -rf feeds/small/trojan-plus
-rm -rf feeds/small/luci-app-trojan-plus
-rm -rf feeds/small/trojan-go
-rm -rf feeds/small/luci-app-trojan-go
+# 1. 强制修改版本定义
+sed -i "s/VERSION_NUMBER:=.*/VERSION_NUMBER:=${VERSION_VAL}/g" include/version.mk
+sed -i "s/VERSION_CODE:=.*/VERSION_CODE:=Stable/g" include/version.mk
 
-# 2. 修改 .config，确保即使之前勾选了也会被取消
-sed -i 's/CONFIG_PACKAGE_luci-app-trojan-plus=y/# CONFIG_PACKAGE_luci-app-trojan-plus is not set/' .config
-sed -i 's/CONFIG_PACKAGE_trojan-plus=y/# CONFIG_PACKAGE_trojan-plus is not set/' .config
-sed -i 's/CONFIG_PACKAGE_luci-app-trojan-go=y/# CONFIG_PACKAGE_luci-app-trojan-go is not set/' .config
-sed -i 's/CONFIG_PACKAGE_trojan-go=y/# CONFIG_PACKAGE_trojan-go is not set/' .config
+# 2. 修改 Web 界面显示
+sed -i "s/DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='OpenWrt ${VERSION_VAL} Stable by Gemini'/g" package/base-files/files/etc/openwrt_release
+
+# 3. 针对 x86_64 的特定优化：修改默认 IP (可选)
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
