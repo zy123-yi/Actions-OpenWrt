678f7b98c827b9a345178314681c8a6584466ea0
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-26 00:20:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-TARGET_FILE="openwrt/feeds.conf.default"
-
-# 1. 强制覆盖为官方 25.12 分支源
-# 这样能保证 scripts/feeds update -a 100% 成功，且不会有语法报错
-echo "src-git packages https://github.com/openwrt/packages.git;openwrt-25.12" > $TARGET_FILE
-echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> $TARGET_FILE
-echo "src-git routing https://github.com/openwrt/routing.git;openwrt-25.12" >> $TARGET_FILE
-echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12" >> $TARGET_FILE
-
-# 2. 注意：这里删除了所有的第三方 src-git (如 small, sbwml)
-# 因为我们要通过 diy-part2.sh 手动克隆，避免 feeds 脚本的严格校验
+# 锁定官方 25.12 稳定版分支
+echo "src-git packages https://github.com/openwrt/packages.git;openwrt-25.12" > feeds.conf.default
+echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> feeds.conf.default
+echo "src-git routing https://github.com/openwrt/routing.git;openwrt-25.12" >> feeds.conf.default
+echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12" >> feeds.conf.default
