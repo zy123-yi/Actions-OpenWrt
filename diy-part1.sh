4cef482b6e5713aa3cadde87aaf1e03bfd0a065b
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 12:20:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+# 添加 PassWall 和 daed 所在的源
+echo 'src-git daed https://github.com/dae-universe/dae-openwrt' >> feeds.conf.default
+# Add third-party feeds
+echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
