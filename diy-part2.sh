6c533357b6906ee5acc4d6999ef378b909389373
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 17:54:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+# 强制从 .config 中取消选中 trojan 相关包，防止由于依赖缺失导致编译失败
+sed -i 's/CONFIG_PACKAGE_luci-app-trojan-plus=y/# CONFIG_PACKAGE_luci-app-trojan-plus is not set/' .config
+sed -i 's/CONFIG_PACKAGE_trojan-plus=y/# CONFIG_PACKAGE_trojan-plus is not set/' .config
