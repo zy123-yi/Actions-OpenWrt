190ef97b1697c3d1c3ab0cb373a8dc4c88c694a3
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 23:51:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 TARGET_FILE="feeds.conf.default"
+# 2. 依然保留你的第三方源 (用于 PassWall)
+echo 'src-git small https://github.com/kenzok8/small' >> $TARGET_FILE
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >> $TARGET_FILE
 
 # 1. 修正官方源：指向 openwrt-25.12 分支，而不是 v25.12.1 标签
 echo "src-git packages https://github.com/openwrt/packages.git;openwrt-25.12" > $TARGET_FILE
@@ -7,6 +10,3 @@ echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> $TARGET
 echo "src-git routing https://github.com/openwrt/routing.git;openwrt-25.12" >> $TARGET_FILE
 echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12" >> $TARGET_FILE
 
-# 2. 依然保留你的第三方源 (用于 PassWall)
-echo 'src-git small https://github.com/kenzok8/small' >> $TARGET_FILE
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >> $TARGET_FILE
