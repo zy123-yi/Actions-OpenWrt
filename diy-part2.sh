27b6afc8465092140125b8c67c0dc5ed1332e983
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 20:41:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#!/bin/bash
 
+# 1. 强制修改版本号显示（把 SNAPSHOT 替换为具体的版本）
+# 这里的 $REPO_BRANCH 会被替换为 openwrt-25.12
+# 我们提取出 25.12 部分
+VERSION_NUM=$(echo $REPO_BRANCH | cut -d '-' -f 2)
+
+sed -i "s/VERSION_NUMBER:=.*/VERSION_NUMBER:=${VERSION_NUM}.0/g" include/version.mk
+sed -i "s/VERSION_CODE:=.*/VERSION_CODE:=Stable/g" include/version.mk
+
+# 2. 修改发布描述，显示在 Web 登录界面
+sed -i "s/DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='OpenWrt ${VERSION_NUM}.0 Stable'/g" package/base-files/files/etc/openwrt_release
 # 1. 从 feeds 源码中彻底删除会导致报错的 trojan 相关包
 rm -rf feeds/small/trojan-plus
 rm -rf feeds/small/luci-app-trojan-plus
