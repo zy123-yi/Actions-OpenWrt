7ad89a93cbf0e3f4a57614fc44fec7bec74095e2
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 15:30:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Add third-party feeds
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-git packages https://github.com/openwrt/packages.git;$REPO_BRANCH" > openwrt/feeds.conf.default
-echo "src-git luci https://github.com/openwrt/luci.git;$REPO_BRANCH" >> openwrt/feeds.conf.default
-echo "src-git routing https://github.com/openwrt/routing.git;$REPO_BRANCH" >> openwrt/feeds.conf.default
-echo "src-git telephony https://github.com/openwrt/telephony.git;$REPO_BRANCH" >> openwrt/feeds.conf.default
-# 使用 >> 表示将内容追加到文件末尾
+# 进入 openwrt 目录 (取决于你的 Workflow 路径)
+# cd openwrt
+
+# 1. 使用 > 覆盖写入第一行，彻底清空原文件并定义官方 packages 分支
+echo "src-git packages https://github.com/openwrt/packages.git;$REPO_BRANCH" > feeds.conf.default
+
+# 2. 使用 >> 追加后续行
+echo "src-git luci https://github.com/openwrt/luci.git;$REPO_BRANCH" >> feeds.conf.default
+echo "src-git routing https://github.com/openwrt/routing.git;$REPO_BRANCH" >> feeds.conf.default
+echo "src-git telephony https://github.com/openwrt/telephony.git;$REPO_BRANCH" >> feeds.conf.default
 # echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
