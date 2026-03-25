410f4e526c2513566df0d6adab8a05b3be580769
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-03-25 14:57:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Add third-party feeds
-
+echo 'src-git packages https://github.com/openwrt/packages.git;openwrt-25.12' >> feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git;openwrt-25.12' >> feeds.conf.default
 # 使用 >> 表示将内容追加到文件末尾
 # echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
-#vecho 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
