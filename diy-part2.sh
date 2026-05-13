eda62e07956c6ac8618088b68b6fc4bb21172b99
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-13 22:42:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,29 +6,30 @@ rm -rf package/community
 mkdir -p package/community
 
 # 2. 拉取 small-package (仅作为备用依赖库)
-git clone --depth 1 https://github.com/kenzok8/small-package.git package/community/small
+# git clone --depth 1 https://github.com/kenzok8/small-package.git package/community/small
 
 # 3. 【关键步骤】删除 small 里的“多刺”插件
 # 删掉 small 里的 passwall, vlmcsd, shadowsocks 等所有容易报错的货
-rm -rf package/community/small/luci-app-passwall
-rm -rf package/community/small/passwall
-rm -rf package/community/small/vlmcsd
-rm -rf package/community/small/luci-app-vlmcsd
-rm -rf package/community/small/shadowsocks*
-rm -rf package/community/small/opkg
+# rm -rf package/community/small/luci-app-passwall
+# rm -rf package/community/small/passwall
+# rm -rf package/community/small/vlmcsd
+# rm -rf package/community/small/luci-app-vlmcsd
+# rm -rf package/community/small/shadowsocks*
+# rm -rf package/community/small/opkg
 
 # 4. 【定向精准拉取】从 jell 仓库只拿你要的“传统 PassWall”和“vlmcsd”
 # 使用这种方式可以保证拿到的包是之前编译成功的那个版本
-git clone --depth 1 --filter=blob:none --sparse https://github.com/kenzok8/jell.git package/community/jell_temp
-cd package/community/jell_temp
-git sparse-checkout set luci-app-passwall vlmcsd luci-app-vlmcsd
+# git clone --depth 1 --filter=blob:none --sparse https://github.com/kenzok8/jell.git package/community/jell_temp
+git clone --depth 1 --filter=blob:none --sparse https://github.com/kenzok8/jell.git
+# cd package/community/jell_temp
+# git sparse-checkout set luci-app-passwall vlmcsd luci-app-vlmcsd
 cd ../../..
 
 # 把 jell 的好包搬出来，放到 package 根目录（优先级最高）
-cp -r package/community/jell_temp/luci-app-passwall package/
-cp -r package/community/jell_temp/vlmcsd package/
-cp -r package/community/jell_temp/luci-app-vlmcsd package/
-rm -rf package/community/jell_temp
+# cp -r package/community/jell_temp/luci-app-passwall package/
+# cp -r package/community/jell_temp/vlmcsd package/
+# cp -r package/community/jell_temp/luci-app-vlmcsd package/
+# rm -rf package/community/jell_temp
 
 # 5. 刷新 feeds 并强制安装（确保依赖链条连通）
 ./scripts/feeds update -a
