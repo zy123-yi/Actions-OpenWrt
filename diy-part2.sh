ed1d42f0575edf48f66caf06e495161b57ba4c27
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-15 06:02:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,7 +25,7 @@ find ./package -type d -name "daed" -exec rm -rf {} +
 find ./feeds -type d -name "daed" -exec rm -rf {} +
 
 # 拉取官方最新标准源到 package/daed
-git clone --depth 1 https://github.com/QiuSimons/luci-app-daed.git package/daed
+# git clone --depth 1 https://github.com/QiuSimons/luci-app-daed.git package/daed
 
 # 1. 彻底清理掉 small-package 里的 vlmcsd（防止它干扰编译）
 # 假设你的目录名是 package/small-package
