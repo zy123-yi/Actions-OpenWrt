452a877eb7179f9d64093acbff7bf8d221c9e575
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-16 09:28:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -89,8 +89,8 @@ find ./ -name "AdGuardHome" -type d -exec rm -rf {} +
 # rm -rf feeds/small/v2ray-plugin
 
 # 5. 额外清理 Turbo ACC 冲突（防止 Duplicate 报错）
-rm -rf feeds/luci/applications/luci-app-turboacc
-rm -rf feeds/packages/net/vlmcsd
+# rm -rf feeds/luci/applications/luci-app-turboacc
+# rm -rf feeds/packages/net/vlmcsd
 
 # 5. 在 .config 中强制禁用这些项目（双重保险）
 # sed -i '/CONFIG_PACKAGE_luci-app-adguardhome/d' .config
