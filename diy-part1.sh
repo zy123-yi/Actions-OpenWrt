cb0ab2c2e1ed0c12c90e80b38703abada1d0d847
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-16 10:32:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,3 @@ echo "src-git packages https://github.com/openwrt/packages.git;openwrt-25.12" > 
 echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> feeds.conf.default
 echo "src-git routing https://github.com/openwrt/routing.git;openwrt-25.12" >> feeds.conf.default
 echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12" >> feeds.conf.default
-echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;mosdns" >> feeds.conf.default
-echo "src-git vlmcsd https://github.com/mchome/luci-app-vlmcsd.git;vlmcsd" >> feeds.conf.default
-echo "src-git turboacc https://github.com/chenmozhijin/turboacc.git;turboacc" >> feeds.conf.default
