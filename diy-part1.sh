2389d4ffeb4660e83ca229fb22924796c2d671d5
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-16 09:19:07 +0800

    Fix mosdns feed entry format in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,4 @@ echo "src-git routing https://github.com/openwrt/routing.git;openwrt-25.12" >> f
 echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12" >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git mosdns" >> feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;mosdns" >> feeds.conf.default
