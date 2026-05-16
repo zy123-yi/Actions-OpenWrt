0bd8de9a9d441a8507f4a4f90e7c14f74507e80b
zy123-yi <68957880+zy123-yi@users.noreply.github.com>
2026-05-16 09:27:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,3 +8,4 @@ echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-pass
 echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> feeds.conf.default
 echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;mosdns" >> feeds.conf.default
 echo "src-git vlmcsd https://github.com/mchome/luci-app-vlmcsd.git;vlmcsd" >> feeds.conf.default
+echo "src-git turboacc https://github.com/chenmozhijin/turboacc.git;turboacc" >> feeds.conf.default
